79995a1ce73ce568c2ee74a45104f676d388375f
TyreseJin <tyresejin3915@gmail.com>
2023-06-24 23:54:26 +0900

    feat: frontend

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,55 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Step Addition</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <header>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <a href="/">Step Addition</a>
         </h1>
+      </header>
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <main className="main">
+        <div className={styles.input_number}>
+          <div>First Number:&nbsp;</div>
+          <input type="number" />
+        </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <div className={styles.input_number}>
+          <div>Second Number:&nbsp;</div>
+          <input type="number" />
+        </div>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+        <div className={styles.generate_button}>
+          <button>Generate Steps</button>
+        </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+        <div className={styles.display_step}>
+          <p>Steps</p>
+        </div>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.save_button}>
+          <button>Save Results to DB</button>
         </div>
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
       <style jsx>{`
         main {
-          padding: 5rem 0;
+          padding: 5vh 20vw;
           flex: 1;
-          display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
+          height: 90vh;
+          width: 100vw;
+          overflow-y: auto;
+          font-size: 16px;
         }
         code {
           background: #fafafa;
@@ -111,5 +75,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
